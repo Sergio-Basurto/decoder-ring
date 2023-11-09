44253dbeb13825b87090c09cb6ea52ba23f74b18
Sergio-Basurto <sergiobasurto0122@gmail.com>
2023-11-08 17:19:48 -0800

    updated proper files

--- src/caesar.js
+++ src/caesar.js
@@ -1,39 +1,61 @@
 const caesarModule = (function () {
+  // Define the caesar function within the caesarModule
   function caesar(input, shift, encode = true) {
+    // Check if the shift value is not present, is 0, or outside the range -25 to 25, return false
     if (!shift || shift === 0 || shift < -25 || shift > 25) {
       return false;
     }
 
+    // Define the alphabet for reference
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    // Convert input text to lowercase to make it case-insensitive
     const inputLowerCase = input.toLowerCase();
+
+    // Initialize an empty string to store the final result
     let result = "";
 
+    // Loop through each character in the input text
     for (let i = 0; i < inputLowerCase.length; i++) {
+      // Get the current character from the input text
       const char = inputLowerCase[i];
 
+      // Check if the character is a letter in the alphabet
       if (alphabet.includes(char)) {
+        // Find the current index of the character in the alphabet
         const currentIndex = alphabet.indexOf(char);
         let newIndex;
 
+        // Calculate the new index after applying the shift
         if (encode) {
+          // For encoding, shift to the right
           newIndex = (currentIndex + shift + 26) % 26;
         } else {
+          // For decoding, shift to the left
           newIndex = (currentIndex - shift + 26) % 26;
         }
 
+        // Get the character from the alphabet at the new index
         const shiftedChar = alphabet[newIndex];
+
+        // Append the shifted character to the result string
         result += shiftedChar;
       } else {
+        // If the character is not in the alphabet (like spaces or punctuation),
+        // add it to the result string as is
         result += char;
       }
     }
 
+    // Return the final encrypted/decrypted string
     return result;
   }
 
+  // Make the caesar function available outside the module
   return {
     caesar,
   };
 })();
 
+// Export the caesar function for use in other modules/files
 module.exports = { caesar: caesarModule.caesar };
--- src/polybius.js
+++ src/polybius.js
@@ -1,5 +1,7 @@
 function polybius(input, encode = true) {
+  // Define the Polybius square as an object with key-value pairs
   const alphabet = {
+    // Each letter maps to a unique string representing its position on the grid
     a: "11",
     b: "21",
     c: "31",
@@ -29,47 +31,62 @@ function polybius(input, encode = true) {
   };
 
   if (encode) {
-    // Encoding logic
-    const encoded = [];
+    // Encoding logic begins here
+    const encoded = []; // Initialize an empty array to hold the encoded characters
+
+    // Loop over each character in the input string, converted to lowercase
     for (let char of input.toLowerCase()) {
       if (char === " ") {
-        encoded.push(" ");
+        encoded.push(" "); // If the character is a space, keep it as a space
       } else if (char in alphabet) {
-        encoded.push(alphabet[char]);
+        // If the character is in our alphabet object, encode it
+        encoded.push(alphabet[char]); // Push the encoded value onto the array
       }
     }
+    // Join the array of encoded values into a single string and return it
     return encoded.join("");
   } else {
-    // Decoding logic
-    // Check if the number of characters excluding spaces is odd
+    // Decoding logic begins here
+
+    // Check if the input length (excluding spaces) is even; if not, return false
     const charCount = input.replace(/ /g, "").length;
     if (charCount % 2 !== 0) {
       return false;
     }
 
+    // Initialize an empty array to hold the decoded characters
     const decoded = [];
-    let i = 0;
+    let i = 0; // Initialize a counter i to 0
+
+    // Loop over the input by index
     while (i < input.length) {
       if (input[i] === " ") {
+        // If the character at index i is a space, add a space to the decoded array
         decoded.push(" ");
-        i++;
+        i++; // Increment i to move to the next character
       } else {
+        // Extract a pair of characters (a two-digit number)
         const char = input.slice(i, i + 2);
+
         if (char === "42") {
+          // If the pair is "42", it could be 'i' or 'j'
           decoded.push("(i/j)");
         } else {
+          // Find which letter corresponds to the pair of digits
           for (let letter in alphabet) {
             if (alphabet[letter] === char) {
-              decoded.push(letter);
-              break;
+              decoded.push(letter); // Add the decoded letter to the array
+              break; // Break out of the loop once the letter is found
             }
           }
         }
+        // Increment i by 2 since we are reading two digits at a time
         i += 2;
       }
     }
+    // Join the array of decoded characters into a single string and return it
     return decoded.join("");
   }
 }
 
-module.exports = { polybius };
+module.exports = { polybius }; // Export the polybius function for use in other files
